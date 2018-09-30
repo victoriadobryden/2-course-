85b9b97f6d80c970dd1d4346cb4709a10b9909c0
Артемий Мысечко <mysechko01@gmail.com>
2018-10-01 02:17:21 +0300

    Constructor/Destructor

---
+++ lab 1/lab 1/Graph.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <vector>
+
+using std::vector;
+
+template<typename T>
+struct Tvertex{
+	T value;
+	vector<Tvertex *> neighbours;
+};
+
+template<typename T>
+class Graph
+{
+	vector<Tvertex<T> *> vertex;
+	int length;
+public:
+	Graph();
+	Graph(int n);
+	~Graph();
+};
+
+template<typename T>
+Graph<T>::Graph()
+{
+	vertex.clear();
+	length = 0;
+}
+
+template<typename T>
+Graph<T>::Graph(int n)
+{
+	vertex.resize(n);
+	length = n;
+}
+
+template<typename T>
+Graph<T>::~Graph()
+{
+	for (int i = 0; i < length; ++i)
+		delete vertex[i];
+	vertex.clear();
+}
